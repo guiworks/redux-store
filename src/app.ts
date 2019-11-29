a41ddab06cad9b9bf910bfae3dfe7d32e644be87
Guilherme Macedo <guilherme.barone@totvs.com.br>
2019-11-29 17:26:25 -0300

    Primeiro commit, com updates

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,48 @@
+
 import { renderTodos } from './utils';
+import { Store } from './store';
+import { reducer} from "./store/reducers";
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
+
+
+let reducers = {
+  todo: reducer
+};
+
+const state = {
+  todos: {
+    loaded: false,
+    loading: false,
+    data: []
+  }
+};
+
+const store = new Store(reducers);
+
+
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
 
-    console.log(payload);
+
+    const payload = { label: input.value, complete: true };
+
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload
+    })
+
 
     input.value = '';
+
+    console.log('store.value', store.value)
   },
   false
 );
